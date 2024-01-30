91ea42c9553ae5d9b2f7df34a26c851ce3b16d80
Nahid <allahparastnahid@gmail.com>
2024-01-30 15:51:41 +0330

    extract course list component

---
+++ src/components/CourseList.jsx
@@ -0,0 +1,43 @@
+const CourseList = () => {
+    return ( <div className="course-list">
+    <div className="course-item">
+      <div className="course-item__image">
+        <img
+          className="course-item__image__img"
+          src="/images/img1.jpg"
+          alt=""
+        />
+      </div>
+      <div className="course-item__detaile">
+        <div className="course-item__body">
+          <div>
+            <p className="title">React.js Course</p>
+            <p className="desc">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+          <span className="rate">4</span>
+        </div>
+        <div className="course-item__footer">
+          <div className="tags">
+            <span className="badge badge--secondary">test1</span>
+            <span className="badge badge--secondary">test2</span>
+          </div>
+          <div className="caption">
+            <div className="date">
+              {new Date().toDateString("en,Us", {
+                year: "numeric",
+                month: "short",
+                day: "numeric",
+              })}
+            </div>
+            <span className="badge badge--primary">completed</span>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  </div> );
+}
+ 
+export default CourseList;
